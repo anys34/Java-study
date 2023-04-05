3f4f12c54fd06813b53e9aba1c01c19c2a741443
anys34 <anyeseong34@gmail.com>
2023-04-05 16:50:48 +0900

    ADD | 4/5 5PM

--- JavaWorkspace/src/chap_07/camera/FactoryCam.java
+++ JavaWorkspace/src/chap_07/camera/FactoryCam.java
@@ -9,4 +9,9 @@ public class FactoryCam  extends Camera { // 자식 클래스 (단 하나의 부
         // 화재 감지
         System.out.println("화재를 감지합니다.");
     }
+
+    @Override // annotation
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 화재 감지");
+    }
 }
