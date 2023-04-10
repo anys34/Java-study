92b20bb07d3a487818135a40ea2960f9646209e8
anys34 <anyeseong34@gmail.com>
2023-04-10 23:54:36 +0900

    ADD | 4/10 12PM

---
+++ JavaWorkspace/src/chap_10/_05_Stream.java
@@ -0,0 +1,11 @@
+package chap_10;
+
+import java.util.Arrays;
+
+public class _05_Stream {
+    public static void main(String[] args) {
+        // 스트림
+        int[] scores = {100, 95, 90, 85, 80};
+        Arrays.stream(scores);
+    }
+}
