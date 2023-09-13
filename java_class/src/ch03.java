1809aa8b379fe7a4b3bb215f11cccb607caf189d
anys34 <anyeseong34@gmail.com>
2023-09-13 17:09:27 +0900

    ADD :: ch03

---
+++ java_class/src/ch03.java
@@ -0,0 +1,19 @@
+public class ch03 {
+    static class A {
+        float sum;
+        void averageScore(int... a) {
+            sum = 0;
+            for(int k : a)
+                sum += k;
+            System.out.println(sum / a.length);
+        }
+    }
+    public static void main(String[] args) {
+        A a = new A();
+
+        a.averageScore(1);
+        a.averageScore(1, 2);
+        a.averageScore(1, 2, 3);
+        a.averageScore(1, 2, 3, 4);
+    }
+}
