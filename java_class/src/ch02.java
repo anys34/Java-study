d93c317cfb4d75c27da4e45deefdca3151e2763a
anys34 <anyeseong34@gmail.com>
2023-09-13 17:03:34 +0900

    ADD :: ch02

---
+++ java_class/src/ch02.java
@@ -0,0 +1,24 @@
+public class ch02 {
+    static class A {
+        boolean a;
+        int b;
+        double c;
+        String d;
+
+        void abc() {
+            System.out.println(b + c);
+            System.out.println(c + d);
+            System.out.println(d + a);
+        }
+    }
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.a);
+        System.out.println(a.b);
+        System.out.println(a.c);
+        System.out.println(a.d);
+        System.out.println();
+
+        a.abc();
+    }
+}
