21ea4500d0ec1e1ccd9410af3316381ab0d355f9
anys34 <anyeseong34@gmail.com>
2023-09-13 17:39:28 +0900

    ADD :: ch05

---
+++ java_class/src/ch05.java
@@ -0,0 +1,16 @@
+public class ch05 {
+    static class A {
+        int m = 3;
+        int n = 5;
+        void abc(int m, int n) {
+            m = this.m;
+            n = n;
+        }
+    }
+    public static void main(String[] args) {
+        A a = new A();
+        a.abc(7, 8);
+        System.out.println(a.m);
+        System.out.println(a.n);
+    }
+}
