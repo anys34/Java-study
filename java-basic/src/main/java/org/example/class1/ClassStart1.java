479d31cb1d3007c25b2ae9e7e085a85480abd36d
anys34 <anyeseong34@gmail.com>
2023-12-21 02:21:09 +0000

    ADD :: 클래스가 필요한 이유

---
+++ java-basic/src/main/java/org/example/class1/ClassStart1.java
@@ -0,0 +1,16 @@
+package org.example.class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String stu1Name = "학생1";
+        int stu1Age = 15;
+        int stu1Grade = 90;
+
+        String stu2Name = "학생2";
+        int stu2Age = 16;
+        int stu2Grade = 80;
+
+        System.out.println("이름 : " + stu1Name + "나이 : " + stu1Age + "성적 : " + stu1Grade);
+        System.out.println("이름 : " + stu2Name + "나이 : " + stu2Age + "성적 : " + stu2Grade);
+    }
+}
