14c86b6e74dc700174549181738692a1eb27ac96
anys34 <anyeseong34@gmail.com>
2023-12-25 23:03:53 +0000

    ADD :: 문제와 풀이

---
+++ java-basic/src/main/java/org/example/class1/ex/MovieReview.java
@@ -0,0 +1,6 @@
+package org.example.class1.ex;
+
+public class MovieReview {
+    String title;
+    String review;
+}
---
+++ java-basic/src/main/java/org/example/class1/ex/ProductOrder.java
@@ -0,0 +1,7 @@
+package org.example.class1.ex;
+
+public class ProductOrder {
+    String productName;
+    int price;
+    int quantity;
+}
